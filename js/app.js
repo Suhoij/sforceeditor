196a95efd8b0a8bf6d00eff488835e02e7e0bd2f
andrey <a2772@mail.ru>
2013-12-17 18:46:25 +0200

    add-del

--- js/app.js
+++ js/app.js
@@ -3,9 +3,6 @@ $(document).ready(function(){
   $(document).foundation();
   MyApp.start({'sf_app_params':{org_id:1,app_id:11,slide_id:111,session_id:1111}});
   MyApp.vent.trigger("getSfparams"); 
-  //chartView=new ChartView()
-  //ch_r=chartView.render()
-  //console.log("chartView.render()= "+ch_r);
 });
 //----------------------MAIN------------------------
 
@@ -21,13 +18,18 @@ MyApp.addInitializer(function(options){
   this.initChartMenu=function() {  
      $(".dropdown >li >a").click(function(){$("#chart-menu >a").text($(this).text())})
   }
-  // do useful stuff here
- //myView = new MyView({
- //   model: options.someModel
- // });
- // MyApp.mainRegion.show(myView);
-  
+ 
 });
+//--------b:-- modal events----------------------------
+$(document).on('open', '[data-reveal]', function () {
+  var modal = $(this);
+  console.log("modal open ",modal[0].id);
+});
+$(document).on('close', '[data-reveal]', function () {
+  var modal = $(this);
+  console.log("modal close ",modal[0].id);
+});
+//--------e:-- modal events----------------------------
 MyApp.vent.on("getSfparams", function(){
   console.table(MyApp.options);
 });
@@ -59,41 +61,9 @@ MyApp.addRegions({
  
 });
 
-//--------------------------------------------------------------------------
-
-   // Chart model
-//     var ChartModel = Backbone.Model.extend({
-//         defaults: {
-//             title: "Chart title",
-// 	    legend: 'true',
-// 	    showlegend: 'true',
-// 	    legendlocation:"left",
-// 	    bordercolor:"black",
-// 	    min:0,
-// 	    max:1,
-// 	    themeName: "Blue",
-// 	    styleName:"ClearBlue",
-// 	    type:"pie",
-// 	    chart_data:[0,1]
-//         }
-//     });
-//   model_c=new ChartModel();
-//   var ChartView = Backbone.Marionette.ItemView.extend({
-// 	template: "#chart-template",
-// 	model:model_c
-//     });
-// 
-
-
-
-
-
-
-
-//-----------------------------Modules------------------------------------
+//-----------------------------MODULES--------------------------------
 MyApp.module("Chart", function(Chart){
-// код модуля
-    // Chart model
+    // ----------------------- Chart models --------------------------
     var ChartModel = Backbone.Model.extend({
         defaults: {
             title: "Chart title",
@@ -111,14 +81,57 @@ MyApp.module("Chart", function(Chart){
       	    chart_data:[['New Name' , '0'], ['New Name1' , '10'], ['New Name2' , '20']]
         }
     });
-    // View
-    var ChartView = Backbone.Marionette.ItemView.extend({
-	//template: "#chart-template",
-	//model:new ChartModel()
+    var DataChartModel = Backbone.Model.extend({
+        defaults: {
+          data_name:"New name ",
+          data_value:0,
+          data_i:0
+        }
     });
-    this.get_model=function(){
-      return this.model;
-    },
+    // ------------------------  Views ---------------------
+    var ChartView = Backbone.Marionette.ItemView.extend({
+	     //template: "#chart-template",
+	     //model:new ChartModel()
+    });
+    var DataItemChartView = Backbone.Marionette.ItemView.extend({
+       events: {
+        'click #data-add-btn': 'addItem',
+        'click .#data-del-btn': 'delItem'    
+        },
+        addItem:function() {alert("Add from dataView")},
+        delItem:function() {alert("Del from dataView")}
+    });
+ 
+    // ------------------------- Controllers ----------------
+    var Controller = Marionette.Controller.extend({
+ 
+        initialize: function(options){
+          //this.stuff = options.stuff;
+        },
+       
+        addData: function(){
+          //this.trigger("data:done", this.stuff);
+          //alert("addData from chart controller");
+          var dt_tpl= MyApp.Chart.data_item_chart_view.render().el.innerHTML;
+          $("#chartDataModal table tbody").append(dt_tpl);
+          $(".data-del").click(function(){
+              $(this).parent().parent().remove();
+          });
+        },
+        delData: function(row_ptr){
+          //this.trigger("data:done", this.stuff);
+          //alert("delData from chart controller");
+          console.log(row_ptr);
+          //$(row_ptr).closest('tr').remove();
+          
+        }
+ 
+    });
+    // this.initEvents = function(master){
+    //     $("#data-add-btn").click(function(){this.addData()})
+    //     $("#data-del-btn").click(function(){this.delData()})
+    // };
+    // ------------------------- Methods --------------------
     this.show_chart=function() {
 	  console.log("Module Chart ->show_chart");
 	 // new ChartView().render();
@@ -129,20 +142,19 @@ MyApp.module("Chart", function(Chart){
 	  $("#chartwidget-code").empty().html("<script>"+chartwidget_code+"</script>");
 	  
     },
+    this.get_model=function(){
+      return this.model;
+    },
+
     Chart.addInitializer(function(){
       this.model=new ChartModel();
       this.chart_view=new ChartView({template: "#chart-template",model:new ChartModel()});
-      
-      //var MyModel =  Backbone.Model.extend();
-      //var myModel = new MyModel({foo: "bar"});
-
-      //MyItemView = Backbone.Marionette.View.extend();
-      //myItemView=new MyItemView({
-	//template: "#myItemTemplate",
-	//model: myModel
-      //});
-
-     // myItemView.render();
+      this.data_chart_model=new DataChartModel();
+      this.data_item_chart_view=new DataItemChartView({template: "#data-chart-item-template",model:this.data_chart_model});
+      this.controller = new Controller();
+      //this.controller.initEvents();
+       $("#data-add-btn").click(function(){MyApp.Chart.controller.addData()})
+       $("#data-del-btn").click(function(){MyApp.Chart.controller.delData()})
     });
   
 });
