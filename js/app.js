37028fb377c5c8f736ebbf3c8f612c74f82bb3c4
andrey <a2772@mail.ru>
2014-02-05 16:02:59 +0200

    home_page set model

--- js/app.js
+++ js/app.js
@@ -453,6 +453,7 @@ MyApp.module("CManager", function(CManager){
                   
           };
           ////////this.se_model.set("blocks_list",blk_obj);
+          console.info('buildSEPage blk_obj=',blk_obj);
           this.home_page_model.set("blocks_list",blk_obj);
           console.log("clmPlaceholderList=",MyApp.CManager.clmPlaceholderList);
           ////////this.se_page_view.model=this.se_model;
